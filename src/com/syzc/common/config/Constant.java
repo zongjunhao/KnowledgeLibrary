9b38cf5a06cfdb12789f709f8bd86c1219cb4e44
zong <1016767058@qq.com>
2019-12-24 20:31:53 +0800

    修改QQ邮箱SMTP使用的端口为587，避免被阿里云封禁

--- src/com/syzc/common/config/Constant.java
+++ src/com/syzc/common/config/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
 
 	// 邮箱相关信息
 	public static final String SMTPHOST = "smtp.qq.com";
-	public static final String MAILPORT = "25";
-	public static final String MAILNAME = "847031946@qq.com";
-	public static final String MAILPSW = "blieeoarwhcpbech";
+	public static final String MAILPORT = "587";
+	public static final String MAILNAME = "1016767058@qq.com";
+	public static final String MAILPSW = "sxfgtjfjzgazbche";
 }
